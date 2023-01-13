6337fd96f75b42d78483c325c4f38d79988551d0
Edson242 <edsonsilveira619@gmail.com>
2023-01-13 19:12:17 -0300

    uptade

--- exercicios/ex48.py
+++ exercicios/ex48.py
@@ -1,8 +1,7 @@
 s = 0
-for c in range(0, 501, 3):
-    print(c)
-    s += c
-print("A soma é igual a {}".format(s))
-print("FIM!!")
-
-# NÃO FINALIZADO!!!!    
+cont = 0
+for c in range(1, 501, 2):
+    if c % 3 == 0:
+        cont =+ 1
+        s =+ c
+print("A soma de todos os {} valores solicitados é {}".format(cont, s))
